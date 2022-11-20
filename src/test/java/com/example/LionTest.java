00aacc642b788f3681ab21cee1070af598befc1c
Mosalev Aleksey <a12-24@yandex.ru>
2022-11-20 15:57:46 +0300

    second check

--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -1,40 +1,67 @@
 package com.example;
 
 import junit.framework.TestCase;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.MockitoAnnotations;
+
 
 import java.util.List;
 
 import static org.mockito.Mockito.when;
 
+@RunWith(Parameterized.class)
 
-@RunWith(MockitoJUnitRunner.class)
 public class LionTest extends TestCase {
-
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
     @Mock
     Felines felines;
     Feline feline;
 
+
+    private final String sex;
+    private final boolean mane;
+
+    public LionTest(String sex, boolean mane) {
+        this.sex = sex;
+        this.mane = mane;
+
+    }
+
+    @Parameterized.Parameters
+
+    public static Object[][] checkTest() {
+        return new Object[][] {
+                {"Самка", false},
+                {"Самец",true},
+        };
+    }
+    @Test
+    public void doesHaveManeReturnTrue() throws Exception {
+
+        Lion lion = new Lion(sex, felines, feline);
+        assertEquals(mane, lion.doesHaveMane());
+    }
+
     @Test
     public void getKittensReturn() throws Exception {
-        Lion lion = new Lion("Самец", felines, feline);
+        Lion lion = new Lion(sex, felines, feline);
         when(felines.getKittens()).thenReturn(1);
         assertEquals(1, lion.getKittens());
     }
 
-    @Test
-    public void doesHaveManeReturnTrue() throws Exception {
-        Lion lion = new Lion("Самка", felines, feline);
-        assertEquals(false, lion.doesHaveMane());
-    }
+
 
     @Test
     public void getFoodReturnPredatorFood() throws Exception {
         Feline feline = new Feline();
-        Lion lion = new Lion("Самец", felines, feline);
+        Lion lion = new Lion(sex, felines, feline);
         var food = lion.getFood();
         var Expected = List.of("Животные", "Птицы", "Рыба");
         assertEquals(Expected, food);
